575b49c635fcef891cff93739a3678db5623c80b
Rodolfo Granata <warlock.cc@gmail.com>
2013-01-13 16:59:23 -0500

    * move customize interface variables out of init.el * load theme

--- emacs.d/init.el
+++ emacs.d/init.el
@@ -1,8 +1,16 @@
 ;; setup el-get package manager
 (add-to-list 'load-path "~/.emacs.d")
 (require 'el-get-setup)
+
+;; keep customization out of our init file
+(when (file-exists-p "~/.emacs.d/custom.el")
+	(setq custom-file "~/.emacs.d/custom.el")
+	(load custom-file))
+
+;; code asistance
 (require 'init-auto-complete)
 
-;; some text editing settings
+;; tunning interface
 (setq default-tab-width 2)
 (defalias 'yes-or-no-p 'y-or-n-p) ; just y/n
+(load-theme 'solarized-dark)
