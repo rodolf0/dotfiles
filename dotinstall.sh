42972b726cdea5a0597dc4beb91b6d2b06e45c53
Rodolfo Granata <warlock.cc@gmail.com>
2020-09-09 18:48:18 -0400

    Tweak vim config - coc.nvim - vim-plug - vim-commentary ....

--- dotinstall.sh
+++ dotinstall.sh
@@ -6,9 +6,11 @@ while read cfg; do
   sfile="$(echo "$cfg" | sed 's/ *:.*$//; s/ *$//')"
   dfile="$(echo "$cfg" | sed 's/^.*: *//; s/^ *//')"
   if [ -e "$HOME/$dfile" ] && ! [[ "$@" =~ force ]]; then
-    echo "Already exists: '$dfile', bailing"
-    exit 1
+    echo "Already exists: '$dfile', skipping"
+    continue
   fi
+  ddir="$(dirname "$HOME/$dfile")"
+  [ -d "$ddir" ] || mkdir -p "$ddir"
   echo "ln -s '$dotdir/$sfile' '~/$dfile'"
   ln -fs "$dotdir/$sfile" "$HOME/$dfile"
 done < "$dotdir/manifest"
