4792c0b587a37dbafa721a2767cb58cf8bf57b06
Rodolfo Granata <warlock.cc@gmail.com>
2021-08-27 17:08:04 -0400

    tune

--- wezterm.lua
+++ wezterm.lua
@@ -16,7 +16,7 @@ return {
     -- {key="H", mods="SHIFT|CTRL", action=wezterm.action{Search={Regex="[a-f0-9]{6,}"}}},
   -- },
 
-  selection_word_boundary = "\t\n{}[]()\"'`,;:",
+  selection_word_boundary = " \t\n{}[]()\"'`,;:",
 
   mouse_bindings = {
     -- Change the default click behavior to only selects text, not to open hyperlinks
